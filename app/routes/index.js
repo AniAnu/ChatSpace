8755e69851b5d12a6b374c014c982a6a071b41c0
Anmol Shah <rockerfame@gmail.com>
2017-05-16 15:17:34 -0300

    Creating route object

---
+++ app/routes/index.js
@@ -0,0 +1,23 @@
+'use strict';
+const router = require('express').Router();
+module.exports = () => {
+    
+    let routes = {
+        
+        'get' : {
+            '/': (req, res, next) => {
+                res.render('login');
+            },
+            '/rooms': (req, res,next) => {
+                res.render('rooms');
+            },
+            '/chat': (req, res, next) => {
+                res.render('chatroom');
+            }
+        },
+        'post': {
+            
+        }
+    }
+    // Iterating through the routes object and mounting the routes
+}
