3416ef558ca527aad06e426250a76f3355bcfe2f
Anmol Shah <rockerfame@gmail.com>
2017-05-16 16:20:30 -0300

    registering helper functions

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,6 @@
 'use strict';
-const router = require('express').Router();
+const h = require('../helpers');
+
 module.exports = () => {
     
     let routes = {
@@ -19,25 +20,6 @@ module.exports = () => {
             
         }
     }
-    // Iterating through the routes object and mounting the routes
-    let registerRoutes = (routes, method) => {
-        
-        for (let key in routes) {
-            if(typeof routes[key] === 'object' && routes[key] !== null && !(routes[key] instanceof Array)) {
-                registerRoutes(routes[key], key)
-                
-            } else {
-                // Registering the routes
-                if(method === 'get') {
-                    router.get(key, routes[key]);
-                } else if(method === 'post') {
-                    router.post(key, routes[key]);
-                }
-            }
-        }
-    }
-    // Invoking the routes
-    registerRoutes(routes);
-    return router;
+  return h.route(routes);
     
 }
