3e74ad9063cc2de6bd0713fb52dfa0b8051ee71b
Anmol Shah <rockerfame@gmail.com>
2017-05-16 15:52:12 -0300

    Mounting the routes

--- app/index.js
+++ app/index.js
@@ -1,14 +1,16 @@
 'use strict';
 
-const router = require('express').Router();
+//const router = require('express').Router();
+//
+//router.get('/', (req, res, next) => {
+//    
+//    //res.send('<h1>Hello Express!</h1>');
+//    //res.sendFile(__dirname + '/views/login.htm');
+//    res.render('login');
+//});
 
-router.get('/', (req, res, next) => {
-    
-    //res.send('<h1>Hello Express!</h1>');
-    //res.sendFile(__dirname + '/views/login.htm');
-    res.render('login');
-});
 
 module.exports = {
-    router: router
+//    router: router
+    router: require('./routes')()
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -20,4 +20,24 @@ module.exports = () => {
         }
     }
     // Iterating through the routes object and mounting the routes
+    let registerRoutes = (routes, method) => {
+        
+        for (let key in routes) {
+            if(typeof routes[key] === 'object' && routes[key] !== null && !(routes[key] instanceof Array)) {
+                registerRoutes(routes[key], key)
+                
+            } else {
+                // Registering the routes
+                if(method === 'get') {
+                    router.get(key, routes[key]);
+                } else if(method === 'post') {
+                    router.post(key, routes[key]);
+                }
+            }
+        }
+    }
+    // Invoking the routes
+    registerRoutes(routes);
+    return router;
+    
 }
