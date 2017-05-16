26e636013d1a79d48cdfb6301aa6fb5693e8ee6f
Anmol Shah <rockerfame@gmail.com>
2017-05-16 12:02:57 -0300

    Initializing Express Middleware

--- server.js
+++ server.js
@@ -2,19 +2,13 @@
 
 const express = require('express');
 const app = express();
+const chatspace = require('./app');
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 
-
-app.get('/', (req, res, next) => {
-    
-    //res.send('<h1>Hello Express!</h1>');
-    //res.sendFile(__dirname + '/views/login.htm');
-    res.render('login');
-});
-
+app.use('/', chatspace.router);
 
 
 app.listen(app.get('port'), () => {
