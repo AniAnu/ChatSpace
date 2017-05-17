25b1915a6f92c9ab280fb82813c79dbee94e46a1
Anmol Shah <rockerfame@gmail.com>
2017-05-17 02:37:50 -0300

    Serialize and deserialize user using passport

--- app/db/index.js
+++ app/db/index.js
@@ -10,6 +10,16 @@ Mongoose.connection.on('error', error => {
     console.log('MongoDB error: ', error);
 });
 
+// Creating a Schema that defines the structure for storing user data
+const chatUser = new Mongoose.Schema({
+    profileId: String,
+    fullName: String,
+    profilePic: String
+});
+
+// Turning the schema into a usable model
+let userModel = Mongoose.model('chatUser', chatUser);
 module.exports = {
-    Mongoose
+    Mongoose,
+    userModel
 }
