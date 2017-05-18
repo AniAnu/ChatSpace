e5f59bb07814e85dbbc86e18e7e470703700be79
Anmol Shah <rockerfame@gmail.com>
2017-05-18 16:44:04 -0300

    Installing Redis

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 'use strict';
-
+const config = require('./config');
+const redis = require('redis').createClient;
+const adapter = require('socket.io-redis');
 //const router = require('express').Router();
 //
 //router.get('/', (req, res, next) => {
@@ -19,6 +21,18 @@ let ioServer = app => {
     app.locals.chatrooms = [];
     const server = require('http').Server(app);
     const io = require('socket.io')(server);
+    io.set('transports', ['websocket']);
+    let pubClient = redis(config.redis.port, config.redis.host, {
+        auth_pass: config.redis.password
+    });
+    let subClient = redis(config.redis.port, config.redis.host, {
+        return_buffers: true,
+        auth_pass: config.redis.password
+    });
+    io.adapter(adapter({
+        pubClient,
+        subClient
+    }));
     io.use((socket, next) => {
         require('./session')(socket.request, {}, next);
     });
@@ -27,7 +41,7 @@ let ioServer = app => {
 }
 
 module.exports = {
-//    router: router
+    //    router: router
     router: require('./routes')(),
     session: require('./session'),
     ioServer
